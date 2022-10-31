2cfd303ac54f74cf87b70faba2a2eaccb750987f
UY Scuti <anshul.sharma05@outlook.com>
2022-10-31 18:15:01 +0530

    Done!

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using Newtonsoft.Json;
+
+namespace csharp_client
+{
+
+    public class Message1 {
+        public string action;
+        public string senderEmail;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string websocketURL = "";
+            using (WebSocket ws = new WebSocket(websocketURL))
+            {
+                Message1 firstMessage = new Message1();
+                firstMessage.action = "setEmail";
+                firstMessage.senderEmail = "hello@gmail.com";
+                string jsonResult = JsonConvert.SerializeObject(firstMessage);
+                Console.WriteLine("Server own message is...");
+                Console.WriteLine(jsonResult);
+                ws.Connect();
+                ws.Send(jsonResult);
+                ws.OnMessage += Ws_OnMessage;
+
+                Console.ReadKey();
+            };
+        }
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Received from server: " + e.Data);
+        }
+    }
+}
